ad84baaa9b0fa2cabb0760767b630dcebd500576
Yaroslav Germanov <hierihon@gmail.com>
2023-10-10 21:53:38 +0300

    Created domain

--- app/src/main/java/com/example/shoppinglist/domain/GetShopListUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopListUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.shoppinglist.domain
 
-class GetShopListUseCase {
-    fun getShoplist(): List<ShopItem>{
-        TODO()
+class GetShopListUseCase(private val shopListRepository: ShopListRepository) {
+    fun getShopList(): List<ShopItem>{
+        return shopListRepository.getShopList()
     }
 }
